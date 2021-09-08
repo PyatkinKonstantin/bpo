45911136dfdcd80bfc82bed6a4b58f2d45251953
PyatkinKonstantin <ptknknst@gmail.com>
2021-09-09 02:42:22 +0300

    0.91

--- server.js
+++ server.js
@@ -3,8 +3,8 @@
    const path = require('path')
    const app = express()
    const PORT = process.env.PORT ?? 3000
-
    app.use(express.urlencoded({extended: true}))
+
    app.post('/create', (req,res)=>{
 
       const user = req.body.title
